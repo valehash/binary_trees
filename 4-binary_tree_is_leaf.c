22fbe2cac9815efec749d0f3a8b4af82cd2f708f
vale <newlechim@gmail.com>
2024-03-26 16:09:54 +0100

    betty doc

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - function to check if a node in a tree is a leaf node
+ * @node : the node to be checked
+ * Return: returns 1 if it's a leaf node and 0 if not
+ */
 int binary_tree_is_leaf(const binary_tree_t *node){
   if (!node)
   {
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_is_root - function to check if a node is the root node or not
+ * @node:The node being checked
+ * Return -Returns 1 if it is the root node and 0 if it isn't
+ */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
   if (!node){
