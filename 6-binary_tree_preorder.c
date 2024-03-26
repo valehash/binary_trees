b78e0963ac049f7bd12ec3fe51a7b7301a6ed7d7
vale <newlechim@gmail.com>
2024-03-26 18:47:53 +0100

    6-binary_tree_preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - function to check if a node in a tree is a leaf node
+ * @node : the node to be checked
+ * Return: returns 1 if it's a leaf node and 0 if not
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)){
+  if (!tree || !func)
+  {
+    return;
+  }
+
+  const binary_tree_t *current_node;
+  current_node = tree;
+
+  func(current_node->n);
+  binary_tree_preorder(current_node->left, func);
+  binary_tree_preorder(current_node->right, func);
+}
