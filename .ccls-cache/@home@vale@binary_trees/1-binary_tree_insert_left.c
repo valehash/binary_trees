f6d9bba4bd7b1b92645510edd75082c9a275f16f
vale <newlechim@gmail.com>
2024-03-26 13:08:20 +0100

    redundancy might be needed

--- .ccls-cache/@home@vale@binary_trees/1-binary_tree_insert_left.c
+++ .ccls-cache/@home@vale@binary_trees/1-binary_tree_insert_left.c
@@ -27,6 +27,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
   if(parent->left != NULL)
   {
     parent->left->parent = newnode;
+     newnode->parent = parent;
+
   }
   
   parent->left = newnode;
