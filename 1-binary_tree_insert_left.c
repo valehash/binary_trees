031e2a8fcaf15bc1b2991fc203bd7076e21afecc
vale <newlechim@gmail.com>
2024-03-28 00:19:25 +0100

    order of operations taken care of in task 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,15 +10,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
   binary_tree_t *newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
     
-  if (newnode == NULL)
-  {
-      return (NULL);
-  }
   if (parent == NULL)
   {
     return (NULL);
   }
-    
+
+  if (newnode == NULL)
+  {
+      return (NULL);
+  }
+   
   newnode->parent = parent;
   newnode->right = NULL;
   newnode->n = value;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,21 +3,22 @@
  * binary_tree_insert_right - function to add a node to the right of a parent
  * @parent: The pointer to the parent node to add a newnode to
  * @value: The value of the node
- *
  * Return :Returns a pointer to the newnode
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
   binary_tree_t *newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
+   if (parent == NULL)
+  {
+    return NULL;
+  }
+
   if (newnode == NULL)
   {
       return NULL;
   }
-  if (parent == NULL)
-  {
-    return NULL;
-  }
+ 
   newnode->parent = parent;
   newnode->left = NULL;
   newnode->n = value;
